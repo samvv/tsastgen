def16895153e78d33ec612d6a09bd03e95b6ac3c
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-06-01 12:24:55 +0200

    Fix CLI to conform with usage in README

--- src/cli.ts
+++ src/cli.ts
@@ -5,95 +5,33 @@ import * as path from "path"
 import ts from "typescript"
 
 import "source-map-support/register"
+import minimist from "minimist"
 
 import generateCode from "./index"
-import { error } from "./util"
+import { error, fatal } from "./util"
 
-const tsArgv = [];
-let inFile: string | null = null;
-let outFile: string | null = null;
+const argv = minimist(process.argv.slice(2));
 
-for (let i = 2; i < process.argv.length; i++) {
-  const arg = process.argv[i];
-  if (arg.startsWith('--out-file=')) {
-    const k = arg.indexOf('=');
-    outFile = arg.substring(k);
-  } else if (arg.startsWith('--out-file')) {
-    i++;
-    if (i >= process.argv.length) {
-      error(`Missing value for --out-file.`);
-      process.exit(1);
-    }
-    outFile = process.argv[i];
-  } else if (!arg.startsWith('-')) {
-    inFile = arg;
+const files = argv._;
+
+const rootNodeName = argv['root-node-name'] ?? 'Syntax'
+
+for (const file of files) {
+  const i = file.indexOf(':');
+  let outFile = null;
+  let inFile;
+  if (i === -1) {
+    inFile = file;
   } else {
-    tsArgv.push(arg);
+    outFile = file.substring(i+1);
+    inFile = file.substring(0, i);
+  }
+  const sourceFile = ts.createSourceFile(inFile, fs.readFileSync(inFile, 'utf8'), ts.ScriptTarget.Latest, true);
+  const generatedCode = generateCode(sourceFile, { rootNodeName });
+  if (outFile === null) {
+    console.error(generatedCode);
+  } else {
+    console.error(`Writing generated AST definitions to ${path.resolve(outFile)} ...`)
+    fs.writeFileSync(outFile, generatedCode, 'utf8');
   }
 }
-
-if (inFile === null) {
-  error(`Must specify exactly one input file.`);
-  process.exit(1);
-}
-
-// const parsedCommandLine = ts.parseCommandLine(tsArgv);
-
-// if (parsedCommandLine.errors.length > 0) {
-//   printDiagnostics(parsedCommandLine.errors);
-//   error(`Invalid command-line arguments passed to compiler invocation.`)
-//   process.exit(1);
-// }
-
-// if (parsedCommandLine.fileNames.length > 1) {
-//   error(`May specify at most one source file to process at a time.`);
-//   process.exit(1);
-// }
-
-const sourceFile = ts.createSourceFile(inFile, fs.readFileSync(inFile, 'utf8'), ts.ScriptTarget.Latest, true);
-const generatedCode = generateCode(sourceFile);
-console.error(generatedCode);
-
-// const program = ts.createProgram(parsedCommandLine.fileNames, { ...parsedCommandLine.options, noEmit: true });
-
-// let emitResult = program.emit(undefined, undefined, undefined, undefined, {
-//   before: [
-//     createTransformer({
-//       isSpecificationFile(fileName) {
-//         return true;
-//       },
-//       getOutputPath(fileName) {
-//         return outFile;
-//       }
-//     })
-//   ],
-// });
-
-// printDiagnostics(ts.getPreEmitDiagnostics(program))
-// printDiagnostics(emitResult.diagnostics);
-
-// if (emitResult.emitSkipped) {
-//   error(`Failed to emit JavaScript souces.`);
-//   process.exit(1);
-// }
-
-// function printDiagnostics(diagnostics: readonly ts.Diagnostic[]) {
-//   diagnostics.forEach(diagnostic => {
-//     if (diagnostic.file) {
-//       let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
-//       let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
-//       console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-//     } else {
-//       console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
-//     }
-//   });
-// }
-
-// function stripExtensions(filename: string) {
-//   const i = filename.indexOf('.');
-//   if (i === -1 || i === 0) {
-//     return filename;
-//   } else{
-//     return filename.substring(0, i);
-//   }
-// }
